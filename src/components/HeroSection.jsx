e4e8bb813c77d3ce106ca4dc0a97860175325760
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2024-07-12 12:55:29 +0530

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const HeroSection = () => {
+    return (
+        <section>
+            <div className="max-w-[1140px] mx-auto px-[12px] bg-[#F5F8F6]">
+                <h3 className='font-kaushan  font-normal text-[32px] leading-[32px] text-[#6E9277] text-center pt-10'>Technology</h3>
+                <h1 className='text-[48px] text-center font-archivo font-semibold max-w-[799px] pt-2.5'>Devices and Treatments to meet your needs</h1>
+
+                <div className="flex flex-row flex-wrap mx-[-12px]">
+                    <div className="w-6/12 px-[12px]">
+
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default HeroSection
